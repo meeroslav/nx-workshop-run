8787360c765e037051a44f442bd16c458b8ac9c1
Miroslav Jonas <missing.manual@gmail.com>
2021-07-02 10:04:41 +0200

    lab 13-2

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,8 +1,13 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (host: Tree, schema: any) {
-  await libraryGenerator(host, { name: schema.name });
+interface Schema {
+  name: string;
+  directory: 'store' | 'api' | 'shared'
+}
+
+export default async function (host: Tree, schema: Schema) {
+  await libraryGenerator(host, { name: `util-${schema.name}`, tags: `type:util, scope:${schema.directory}`, directory: schema.directory });
   await formatFiles(host);
   return () => {
     installPackagesTask(host);
