769b1e81e2cea4d6ab02386d2e3da51c9e31ee65
Miroslav Jonas <missing.manual@gmail.com>
2021-07-02 13:57:20 +0200

    lab 17-3

--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.spec.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { of } from 'rxjs';
 
 import { GameDetailComponent } from './game-detail.component';
 
@@ -7,10 +10,21 @@ describe('GameDetailComponent', () => {
   let fixture: ComponentFixture<GameDetailComponent>;
 
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      declarations: [ GameDetailComponent ]
+      declarations: [GameDetailComponent],
+      imports: [RouterModule.forChild([]), HttpClientTestingModule],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            paramMap: of()
+          }
+        }
+      ]
+
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
